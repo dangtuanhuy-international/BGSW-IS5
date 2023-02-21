5d02bf1e6d7a1b5f0836f6a1e6dd58e29351f9d8
APAC\DAG8HC <dang8hc@bosch.com>
2023-02-21 17:13:46 +0700

    Init

---
+++ BGSV.IDP/HostingExtensions.cs
@@ -0,0 +1,47 @@
+using Serilog;
+
+namespace BGSV.IDP;
+
+internal static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        // uncomment if you want to add a UI Using UI of ISUI
+        builder.Services.AddRazorPages();
+
+        builder.Services.AddIdentityServer(options =>
+            {
+                // https://docs.duendesoftware.com/identityserver/v6/fundamentals/resources/api_scopes#authorization-based-on-scopes
+                options.EmitStaticAudienceClaim = true;
+            })
+            .AddInMemoryIdentityResources(Config.Ids)
+            .AddInMemoryApiResources(Config.Apis)
+            .AddInMemoryApiScopes(Config.ApiScopes)
+            .AddInMemoryClients(Config.Clients)
+            .AddTestUsers(TestUsers.Users);
+                 //https://localhost:5005/.well-known/openid-configuration
+        return builder.Build();
+    }
+    
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    { 
+        app.UseSerilogRequestLogging();
+    
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        // uncomment if you want to add a UI
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseIdentityServer();
+
+        // uncomment if you want to add a UI
+        app.UseAuthorization();
+        app.MapRazorPages().RequireAuthorization();
+
+        return app;
+    }
+}
